8f6c9a402d0f372a1e9e571eb251e0d3b35201ab
Wyrmnax <wyrmnax@gmail.com>
2017-07-29 09:59:48 -0300

    Enemy Scripts

--- Roguelike/Assets/Scripts/GameManager.cs
+++ Roguelike/Assets/Scripts/GameManager.cs
@@ -1,13 +1,18 @@
-﻿using UnityEngine;
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
 
 public class GameManager : MonoBehaviour {
 
+    public float turnDelay = .1f;
     public static GameManager instance = null;
     public BoardManager boardScript;
     public int playerFoodPoints = 100;
     [HideInInspector] public bool playersTurn = true;
 
     private int level = 45;
+    private List<Enemy> enemies;
+    private bool enemiesMoving;
 
 	// Use this for initialization
 	void Awake () {
@@ -17,6 +22,7 @@ public class GameManager : MonoBehaviour {
             Destroy(gameObject);
 
         DontDestroyOnLoad(gameObject);
+        enemies = new List<Enemy>();
         boardScript = GetComponent<BoardManager>();
         InitGame();
 		
@@ -24,6 +30,7 @@ public class GameManager : MonoBehaviour {
 
     void InitGame()
     {
+        enemies.Clear();
         boardScript.SetupScene(level);
 
     }
@@ -35,6 +42,35 @@ public class GameManager : MonoBehaviour {
 	
 	// Update is called once per frame
 	void Update () {
-		
+		if (playersTurn || enemiesMoving)
+        {
+            return;
+        }
+
+        StartCoroutine(MoveEnemies());
 	}
+
+    public void AddEnemyToList (Enemy script)
+    {
+        enemies.Add(script);
+    }
+
+    IEnumerator MoveEnemies()
+    {
+        enemiesMoving = true;
+        yield return new WaitForSeconds(turnDelay);
+        if (enemies.Count == 0)
+        {
+            yield return new WaitForSeconds(turnDelay);
+        }
+
+        for (int i = 0; i< enemies.Count; i++)
+        {
+            enemies[i].MoveEnemy();
+            yield return new WaitForSeconds(enemies[i].moveTime);
+        }
+
+        playersTurn = true;
+        enemiesMoving = false;
+    }
 }
